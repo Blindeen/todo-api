e97a379240c89b352523f812aae9956b5014cc3d
Blindeen <kuba.szmajda@gmail.com>
2023-09-14 08:19:40 +0200

    add min length for password

--- todo_api/serializers.py
+++ todo_api/serializers.py
@@ -4,7 +4,7 @@ class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=20)
     name = serializers.CharField(max_length=20)
     email = serializers.EmailField(max_length=100)
-    password = serializers.CharField(max_length=100)
+    password = serializers.CharField(min_length=8, max_length=100)
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
