325973945294616bcddee5db3eb67595d4e7cdaa
Blindeen <kuba.szmajda@gmail.com>
2023-09-14 14:19:15 +0200

    add constants to classes

--- todo_api/models.py
+++ todo_api/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserData(AbstractUser):
+    NAME_MAX_LEN = 100
+    EMAIL_MAX_LEN = 100
+
     username = None
-    name = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=NAME_MAX_LEN, unique=True)
+    email = models.EmailField(max_length=EMAIL_MAX_LEN, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
@@ -15,13 +18,16 @@ class UserData(AbstractUser):
 
 
 class List(models.Model):
-    header = models.CharField(max_length=20)
+    HEADER_MAX_LEN = 20
 
+    header = models.CharField(max_length=HEADER_MAX_LEN)
     user = models.ForeignKey(UserData, on_delete=models.CASCADE)
 
 
 class Task(models.Model):
-    description = models.CharField(max_length=100)
+    DESCRIPITON_MAX_LEN = 100
+
+    description = models.CharField(max_length=DESCRIPITON_MAX_LEN)
     is_done = models.BooleanField(default=False)
 
     list = models.ForeignKey(List, on_delete=models.CASCADE)
