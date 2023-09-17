3720015e66ca5febefb8e56ea28ec7212b69be5f
Blindeen <kuba.szmajda@gmail.com>
2023-09-17 19:42:11 +0200

    format code

--- todo_api/serializers.py
+++ todo_api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
+
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=20)
     name = serializers.CharField(max_length=20)
     email = serializers.EmailField(max_length=100)
     password = serializers.CharField(min_length=8, max_length=100)
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
--- todo_api/views.py
+++ todo_api/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from todo_api import serializers, models, services, exceptions
+from todo_api import serializers, services
 
 
 class RegisterView(APIView):
