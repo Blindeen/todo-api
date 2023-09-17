65396f585ab41e1b4b675fe87ac85da8669357b8
Blindeen <kuba.szmajda@gmail.com>
2023-09-17 19:41:36 +0200

    move register and login logic to service

--- todo_api/services.py
+++ todo_api/services.py
@@ -1,4 +1,48 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from todo_api import models, exceptions
+
+
+class LoginRegisterService:
+    @staticmethod
+    def register_user(data):
+        user = models.UserData.objects.create(
+            name=data["name"],
+            email=data["email"],
+        )
+
+        user.set_password(data["password"])
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+        response_data = {
+            "token": str(refresh.access_token),
+            "name": user.name,
+            "email": user.email,
+        }
+
+        return response_data
+
+    @staticmethod
+    def login_user(data):
+        email = data["email"]
+        password = data["password"]
+        try:
+            user = models.UserData.objects.get(email=email)
+        except models.UserData.DoesNotExist:
+            raise exceptions.IncorrectEmailException
+
+        if not user.check_password(password):
+            raise exceptions.IncorrectPasswordException
+
+        refresh = RefreshToken.for_user(user)
+        response_data = {
+            "token": str(refresh.access_token),
+            "name": user.name,
+            "email": user.email,
+        }
+
+        return response_data
 
 
 class ListService:
--- todo_api/views.py
+++ todo_api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from todo_api import serializers, models, services, exceptions
 
@@ -13,21 +12,8 @@ class RegisterView(APIView):
         serializer = serializers.RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        user = models.UserData.objects.create(
-            name=serializer.validated_data["name"],
-            email=serializer.validated_data["email"],
-        )
-
-        user.set_password(serializer.validated_data["password"])
-        user.save()
-
-        refresh = RefreshToken.for_user(user)
-        data = {
-            "token": str(refresh.access_token),
-            "name": user.name,
-            "email": user.email,
-        }
-        return Response(data, status=status.HTTP_201_CREATED)
+        response_data = services.LoginRegisterService.register_user(serializer.validated_data)
+        return Response(response_data, status=status.HTTP_201_CREATED)
 
 
 class LoginView(APIView):
@@ -37,23 +23,8 @@ class LoginView(APIView):
         serializer = serializers.LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        email = serializer.validated_data["email"]
-        password = serializer.validated_data["password"]
-        try:
-            user = models.UserData.objects.get(email=email)
-        except models.UserData.DoesNotExist:
-            raise exceptions.IncorrectEmailException
-
-        if not user.check_password(password):
-            raise exceptions.IncorrectPasswordException
-
-        refresh = RefreshToken.for_user(user)
-        data = {
-            "token": str(refresh.access_token),
-            "name": user.name,
-            "email": user.email,
-        }
-        return Response(data, status=status.HTTP_200_OK)
+        response_data = services.LoginRegisterService.login_user(serializer.validated_data)
+        return Response(response_data, status=status.HTTP_200_OK)
 
 
 class CreateListView(APIView):
